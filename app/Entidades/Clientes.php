85392887c37532e9245efb11b66a0bcdec6bf000
luroma88 <93597316+luroma88@users.noreply.github.com>
2022-05-10 17:02:03 -0300

    cambios

--- app/Entidades/Clientes.php
+++ app/Entidades/Clientes.php
@@ -25,35 +25,92 @@ class Cliente extends Model
     ];
 
     public function cargarDesdeRequest($request) {
+      $this->idcliente = $request->input('id') != "0" ? $request->input('id') : $this->idcliente;
+      $this->nombre = $request->input('txtNombre');
+      $this->apellido = $request->input('txtApellido');
+      $this->telefono = $request->input('txtTelefono');
+      $this->correo = $request->input('txtCorreo');
+      $this->clave = $request->input('txtClave');
 
     }
 
     public function obtenerTodos()
     {
         $sql = "SELECT
-                  A.idmenu,
+                  A.idcliente,
                   A.nombre
-                FROM sistema_menues A ORDER BY A.nombre";
+                FROM clientes A ORDER BY A.nombre";
         $lstRetorno = DB::select($sql);
         return $lstRetorno;
     }
 
-    public function obtenerPorId($idmenu)
+    public function obtenerPorId($idcliente)
     {
+            $sql = "SELECT
+                   'idcliente',
+                   'nombre',
+                   'apellido',
+                   'telefono',
+                   'correo',
+                   'clave',
+                    FROM clientes WHERE idcliente = $idcliente";
+            $lstRetorno = DB::select($sql);
+
+      if (count($lstRetorno) > 0) {
+            $this->idcliente = $lstRetorno[0]->idcliente;
+            $this->nombre = $lstRetorno[0]->nombre;
+            $this->apellido = $lstRetorno[0]->apellido;
+            $this->telefono = $lstRetorno[0]->telefono;
+            $this->correo = $lstRetorno[0]->correo;
+            $this->clave = $lstRetorno[0]->clave;
+      
+            return $this;
+      }
+      return null;
 
     }
     public function guardar() { /* forma correcta que propone larabel para evitar inyecciones de querys maliciosas */
-    }
+            $sql = "UPDATE clientes SET
+                  idcliente=?,
+                  nombre=?,
+                  apellido=?,
+                  telefono=?,
+                  correo=?,
+                  clave=?
+                  WHERE idcliente=?";
+            $affected = DB::update($sql, 
+            [$this->idcliente,
+            $this->nombre,
+            $this->apellido,
+            $this->telefono,
+            $this->correo,
+            $this->clave]);
+}
     
     public function eliminar()
     {
-        $sql = "DELETE FROM sistema_menues WHERE
-            idmenu=?";
-        $affected = DB::delete($sql, [$this->idmenu]);
+        $sql = "DELETE FROM clientes WHERE
+            idcliente=?";
+        $affected = DB::delete($sql, [$this->idcliente]);
     }
 
     public function insertar()
     {
+      $sql = "INSERT INTO sistema_menues (
+            nombre,
+            apellido,
+            telefono,
+            correo,
+            clave
+        ) VALUES (?, ?, ?, ?, ?);";
+      $result = DB::insert($sql, [
+            $this->nombre,
+            $this->apellido,
+            $this->telefono,
+            $this->correo,
+            $this->clave
+      ]);
+      return $this->idcliente = DB::getPdo()->lastInsertId(); // accede al ultimo insertado
     }
     
 }
